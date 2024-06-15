54f45c25e8a52e2c2864a1f1dc7a38c67608b967
Michael Kenny <106249795+KennyPressF@users.noreply.github.com>
2024-06-15 12:00:07 +0100

    Created singleton pattern and added to Player class

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : SingletonMonobehaviour<Player>
+{
+    //public static Player Instance;
+
+    //void Awake()
+    //{
+    //    if (Instance == null)
+    //    {
+    //        Instance = this;
+    //        DontDestroyOnLoad(this.gameObject);
+    //    }
+    //    else if (Instance != this)
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+}

ce8f30ce8cbe0d62cf5ca967c458534b1958dec2
Michael Kenny <106249795+KennyPressF@users.noreply.github.com>
2024-06-18 22:02:01 +0100

    Finished rudimentary dash and stamina system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : PlayerInputManager
 {
     float baseSpeed;
-    float speedVariable = 1f;
+    public float speedVariable;
+    [SerializeField] float currentStamina;
+    [SerializeField] float maxStamina;
+    [SerializeField] float staminaRegenRate;
+    [SerializeField] float staminaDepletionRate;
+    bool isDashing = false;
+
+    // Current stamina variable
 
     Vector2 moveInputValue;
 
@@ -21,51 +28,94 @@ public class PlayerMovement : PlayerInputManager
     private void Start()
     {
         baseSpeed = player.MoveSpeed;
+        currentStamina = maxStamina;
     }
 
     protected override void OnEnable()
     {
         base.OnEnable();
-        base.PlayerInput.Player.Move.performed += OnMovePerformed;
-        base.PlayerInput.Player.Move.canceled += OnMoveCanceled;
-        base.PlayerInput.Player.Dash.started += OnDashStarted;
+        base.PlayerInput.Player.Move.performed += OnMove;
+        base.PlayerInput.Player.Move.canceled += OnMove;
+        base.PlayerInput.Player.Dash.performed += OnDash;
+        base.PlayerInput.Player.Dash.canceled += OnDash;
         player.OnMoveSpeedChanged += UpdateBaseMoveSpeed;
     }
 
     protected override void OnDisable()
     {
-        base.PlayerInput.Player.Move.performed -= OnMovePerformed;
-        base.PlayerInput.Player.Move.canceled -= OnMoveCanceled;
-        base.PlayerInput.Player.Dash.canceled -= OnDashCanceled;
+        base.PlayerInput.Player.Move.performed -= OnMove;
+        base.PlayerInput.Player.Move.canceled -= OnMove;
+        base.PlayerInput.Player.Dash.performed -= OnDash;
+        base.PlayerInput.Player.Dash.canceled -= OnDash;
         base.OnDisable();
         player.OnMoveSpeedChanged -= UpdateBaseMoveSpeed;
     }
 
-    private void OnMovePerformed(InputAction.CallbackContext context)
+    private void Update()
     {
-        moveInputValue = context.ReadValue<Vector2>();
+        ProcessMovement();
+        ProcessDash();
     }
 
-    private void OnMoveCanceled(InputAction.CallbackContext context)
+    private void OnMove(InputAction.CallbackContext context)
     {
-        moveInputValue = Vector2.zero;
+        if (context.performed)
+        {
+            moveInputValue = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            moveInputValue = Vector2.zero;
+        }
     }
 
-    private void OnDashStarted(InputAction.CallbackContext context)
-    {
-        Debug.Log("Dash started");
-    }
-
-    private void OnDashCanceled(InputAction.CallbackContext context)
-    {
-        Debug.Log("Dash canceled");
-    }
-
-    private void FixedUpdate()
+    private void ProcessMovement()
     {
         float moveSpeed = baseSpeed * speedVariable;
         Vector3 moveDir = new Vector3(moveInputValue.x, moveInputValue.y, 0);
         transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Dasking = TRUE");
+            isDashing = true;
+        }
+        else if (context.canceled)
+        {
+            Debug.Log("Dasking = FALSE");
+            isDashing = false;
+        }
+    }
+
+    private void ProcessDash()
+    {
+        if (isDashing == false && currentStamina >= maxStamina)
+        {
+            speedVariable = 1f;
+            return;
+        }
+
+        if (isDashing && currentStamina <= 0)
+        {
+            speedVariable = 1f;
+            return;
+        }
+
+        if (isDashing)
+        {
+            speedVariable = 2f;
+            currentStamina -= staminaDepletionRate * Time.deltaTime;
+        }
+        else
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            speedVariable = 1f;
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
     }
 
     private void UpdateBaseMoveSpeed(float newBaseSpeed)
